9702a51378ff54d6a52fe066f40cc8d4296d0a98
madhankb <madhankb94@gmail.com>
2025-08-08 15:51:01 -0700

    Clean-up

--- run_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Activate virtual environment and run the script
-source venv/bin/activate
-/Users/mkbn/Public/AOS-manual-snapshot-automation/venv/bin/python create_iam_resources.py "$@"
